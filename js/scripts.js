4e8a4cd15d4055cc9a7d2e1b84175c684a182200
Abe Coutinho <abrcoutinho@gmail.com>
2024-12-02 03:43:50 -0300

    Adicionado fluxo de desmobilizar ativos

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,46 @@ function toggleTheme(e) {
 switchTheme.addEventListener('change', toggleTheme, false);
 
 
-// Desmob cards
+// Tabs em detalhe do contrato
+function changeAssetsTab(evt, content) {
+  var i,  tabcontent, tablink;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablink = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablink.length; i++) {
+    tablink[i].className = tablink[i].className.replace(" active", "");
+  }
+
+  document.getElementById(content).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+// Desmobilizar ativos em detalhe do contrato
+function revealSelectedAssets() {
+  document.querySelector(".contract_detail > aside").style.display = "none";
+  document.querySelector(".contract_detail .ativos_selecionados").style.display = "flex";
+}
+function cancelSelectedAssets() {
+  document.querySelector(".contract_detail > aside").style.display = "flex";
+  document.querySelector(".contract_detail .ativos_selecionados").style.display = "none";
+}
+function confirmDesmobSelectedAssets() {
+  document.querySelector(".contract_detail .ativos_selecionados .dialog").style.display = "block";
+}
+function cancelDesmobDialog() {
+  document.querySelector(".contract_detail .ativos_selecionados .dialog").style.display = "none";
+}
+function selectMotivo() {
+  document.querySelector(".contract_detail .ativos_selecionados .motivo_desmob_menu").style.display = "block";
+}
+
+
+// Expande cards da esteira de desmobilizacao
 function expandDesmobCard(evt) {
   var i, desmob_card;
 
@@ -22,6 +61,5 @@ function expandDesmobCard(evt) {
     desmob_card[i].className = desmob_card[i].className.replace(" expanded", "");
   }
   evt.currentTarget.className += " expanded";
-
 }
 
